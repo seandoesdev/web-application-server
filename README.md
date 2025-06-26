84cd7f7ab264ecf7efc299ee22c0d83ecf93f106
seandoesdev <seandoesdev@gmail.com>
2025-06-26 23:57:13 +0900

    요구사항 1 - index.html 응답하기

--- README.md
+++ README.md
@@ -15,7 +15,18 @@
 * 각 요구사항을 구현하는 것이 중요한 것이 아니라 구현 과정을 통해 학습한 내용을 인식하는 것이 배움에 중요하다. 
 
 ### 요구사항 1 - http://localhost:8080/index.html로 접속시 응답
-* 
+
+#### 구현내용
+* http://localhost:8080/index.html로 접속시 응답하는 내용은 webapp/index.html 파일에 작성한다.
+* webapp/index.html 파일에 작성한 내용을 http://localhost:8080/index.html로 접속하면 응답한다.
+
+#### 배운점
+* 이 웹 서버 구현에서 소켓통신으로 요청이 들어오면, 해당 요청에 대한 쓰레드를 생성하고 start() 메서드 호출로 run() 메서드를 실행한다.
+* connection.getInputStream()으로 데이터를 읽어오는데, InputStreamReader로 사용해서 문자 단위로 데이터를 처리해서 가변적인 문자열을 매번 입력 받기에는 불편함이 있다. 따라서, 이 웹 서버 구현에서는 BufferedReader를 사용한다.
+    * BufferedReader를 사용하면 한번에 한 줄씩 읽어올 수 있어서, 한번에 읽어온 데이터를 문자열로 변환하는 것이 더 효율적이다.
+* readAllBytes() :입력 스트림(InputStream)이나 파일(Path)의 전체 내용을 한 번에 바이트 배열로 읽어들이는 메서드
+    * 주의할점 : 큰 용량의 파일은 메모리에 한 번에 모두 읽어들여야 하므로, 메모리 부족 문제가 발생할 수 있다.
+    * 큰 파일 처리는 read() 메서드나 readLine() 메서드 등을 사용해야 한다.
 
 ### 요구사항 2 - get 방식으로 회원가입
 * 
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,14 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,8 +28,30 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String requestLine = br.readLine();
+
+            if(requestLine == null) {
+                return;
+            }
+
+            String[] tokens = requestLine.split(" ");
+
+            // 헤더 마지막 확인
+            while(!"".equals(requestLine)){
+                requestLine = br.readLine();
+                log.info("requestLine => {}", requestLine);
+            }
+
+            // 응답
+            String url = tokens[1];
+            if(url.equals("/")) {
+                url = "/index.html";
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+            // byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
